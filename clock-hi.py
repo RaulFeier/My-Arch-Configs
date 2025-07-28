13bb35220afea637be8027b3f067231b9d5f694d
RaulFeier <ionelafeier1@gmail.com>
2025-07-28 16:20:30 +0300

    eep

---
+++ clock-hi.py
@@ -0,0 +1,17 @@
+import datetime
+
+def main():
+    date = datetime.datetime.now()
+    
+    hour = date.hour
+    
+    if hour >= 0 and hour <= 7:
+        print("Work Hard ")
+    elif hour >= 8 and hour <= 14:
+        print("Good Morning Misu")
+    elif hour >= 15 and hour <= 19:
+        print("Good Afternoon Misu")
+    else:
+        print("Good Evening Misu")
+
+main()
