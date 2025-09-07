87496d16be6134213724d21a681f6e68010384b2
RaulFeier <ionelafeier1@gmail.com>
2025-09-08 00:38:43 +0300

    eep

--- clock-hi.py
+++ clock-hi.py
@@ -6,12 +6,12 @@ def main():
     hour = date.hour
     
     if hour >= 0 and hour <= 7:
-        print("Work Hard ")
+        print("Work Hard 🐧")
     elif hour >= 8 and hour <= 14:
-        print("Good Morning Misu")
+        print("Good Morning Misu 🐧")
     elif hour >= 15 and hour <= 19:
-        print("Good Afternoon Misu")
+        print("Good Afternoon Misu 🐧")
     else:
-        print("Good Evening Misu")
+        print("Good Evening Misu 🐧")
 
 main()
