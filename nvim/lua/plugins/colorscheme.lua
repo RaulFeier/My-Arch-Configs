61224c5476b076f2e7cc8d7ae8f38c296f4b6ccd
RaulFeier <ionelafeier1@gmail.com>
2026-05-24 19:39:39 +0300

    eep

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "min-dark",
+    },
+  },
+  {
+    "AlexvZyl/nordic.nvim",
+    -- lazy = false,
+    -- priority = 1000,
+    -- config = function()
+    --   require("nordic").load()
+    -- end,
+  },
+  {
+    "marko-cerovac/material.nvim",
+  },
+  {
+    "scottmckendry/cyberdream.nvim",
+  },
+  {
+    "wesleimp/min-theme.nvim",
+  },
+}
